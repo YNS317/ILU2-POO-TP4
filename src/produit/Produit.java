2080543aeb2fd872ee977d1eed98783f5d3264fd
YNS317 <145299495+YNS317@users.noreply.github.com>
2024-04-03 11:56:33 +0200

    TP4 a finir

--- src/produit/Produit.java
+++ src/produit/Produit.java
@@ -1,5 +1,15 @@
 package produit;
 
-public class Produit {
+public abstract class Produit {
+    private String nom;
 
+    public Produit(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public abstract String description();
 }
