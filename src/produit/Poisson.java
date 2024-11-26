4a1415bddef314d61234210da2b36186266e0540
YNS317 <145299495+YNS317@users.noreply.github.com>
2024-11-26 19:09:58 +0100

    tp4 presque fini

---
+++ src/produit/Poisson.java
@@ -0,0 +1,19 @@
+package produit;
+
+public class Poisson extends Produit{
+	private String date;
+	public Poisson(String date) {
+		super("poisson");
+		this.date=date;
+
+	}
+	@Override
+	public String decrireProduit() {
+		return nom+" péchés "+ date+".";
+	}
+	@Override
+	public int calculerPrix(int prix) {
+		return prix;
+	}
+
+}
